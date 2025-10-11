d7fbda270879c9b50db5acf662edbc6cc3b48f8e
alaa algharbi <algarbealaa@gmail.com>
2025-10-11 15:43:34 +0000

    Finish pagination and django filter

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,8 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.value
-
+    class Meta:
+        ordering = ["value"]
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
